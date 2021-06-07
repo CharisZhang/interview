d7f8731beb2a93d8c0ee4d01ec445678f99bd0bb
张超 <zhangchao@zhangchaodeMacBook-Pro.local>
2021-06-08 00:25:09 +0800

    z

--- src/index.js
+++ src/index.js
@@ -4,22 +4,48 @@ function object(o) {
   F.prototype = o
   return new F()
 }
-
-function prototype(child, parent) {
+function prototype(child,parent) {
   var prototype = object(parent.prototype)
   prototype.constructor = child
   child.prototype = prototype
 }
 
-function Parent() {
-  this.name = 'parent'
+
+
+
+
+
+
+
+function Parent1() {
+  // console.log('Parent1');
+  this.name = 'parent1'
   this.arr = [1,2,3]
 }
-Parent.prototype.getName = function() {
+Parent1.prototype.name = 'pro_parent1'
+Parent1.prototype.getName = function() {
   return this.name
 }
-function Child() {
-  
+function Child1() {
+  // console.log('Child1');
 }
-prototype(Child,Parent)
-var child = new Child()
+prototype(Child1,Parent1)
+
+var child1 = new Child1()
+var parent1 = new Parent1()
+console.log(child1.getName());
+
+
+class Parent2 {
+  static name = 'Parent2'
+  constructor(){
+    // console.log('Parent2');
+  }
+}
+class Child2 extends Parent2{
+  constructor(){
+    super()
+  }
+}
+var child2 = new Child2()
+// console.log(child2);
