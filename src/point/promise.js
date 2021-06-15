a19d293be909de482f1f5fe9172ee189206db9c2
charis <tochariszhang@sina.com>
2021-06-15 14:40:04 +0800

    z

--- src/point/promise.js
+++ src/point/promise.js
@@ -73,43 +73,42 @@
 
 
 
-/**
-new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve({test:1})
-    resolve({test:2})
-    reject({ test: 3 })
-  }, 500)
-}).then((res) => {
-    console.log(res)
-}, (reason) => {
-    console.log('1' + reason) // 出错了！
-}).then((res) => {
-    console.log('2' + res)
-})
+// new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve({test:1})
+//     resolve({test:2})
+//     reject({ test: 3 })
+//   }, 500)
+// }).then((res) => {
+//     console.log(res)
+// }, (reason) => {
+//     console.log('1' + reason) // 出错了！
+// }).then((res) => {
+//     console.log('2' + res)
+// })
 let p1 = new Promise((resolve,reject)=>{
     setTimeout(() => {
-        reject({name:'p1-resolve'})
+        resolve({name:'p1-resolve'})
     }, 500);
 })
 let p2 = new Promise((resolve,reject)=>{
     setTimeout(() => {
-        reject({name:'p2-reject'})
+        resolve({name:'p2-reject'})
     }, 1000);
 })
 let p3 = new Promise((resolve,reject)=>{
     setTimeout(() => {
-        reject({name:'p3-resolve'})
+        resolve({name:'p3-resolve'})
     }, 1500);
 })
 // Promise
 // p2.catch((e)=>{console.log(e);})
+// const p = Promise.all([p1, p2, p3])
+// const p = Promise.race([p1, p2, p3])
+// const p = Promise.allSettled([p1, p2, p3])
 const p = Promise.any([p1, p2, p3])
 p.then((res)=>{
-    console.log('then');
-    console.log(res);
+    console.log('then',res);
 }).catch((e)=>{
-    console.log('catch');
-    console.log(e);
+    console.log('catch',e);
 })
- */
