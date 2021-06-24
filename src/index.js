08ef98de5ba9d3c926d206b7fe9bd448f69470e5
charis <tochariszhang@sina.com>
2021-06-24 09:48:17 +0800

    z

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-// Function.prototype._call = function(context) {
-//   // var context = context || window
-//   context.fn = this
-//   var args = []
-//   for (let i = 1; i < arguments.length; i++) {
-//     args.push('arguments['+i+']')
-//   }
-//   eval('context.fn('+args+')')
-  
-// }
-
-// var n = 1
-// function fn(m) {
-//   console.log('n',this.n);
-//   console.log('m',m);
-// }
-// var obj = {
-//   n: 2
-// }
-// // fn(22)
-// fn._call()
-// // fn.apply(obj,[22])
+function Company() {
+  var instance = {
+    name: 'a1',
+    getName() {
+      console.log(this.name);
+    },
+    getName1: () => {
+      console.log(this.name);
+    }
+  };
+  this.name = 'a2';
+  this.getName2 = function () {
+    console.log(this.name);
+  }
+  return instance;
+}
+Company.prototype.name = 'a3';
+var company1 = new Company();
+console.log(company1);
+company1.getName();
+company1.getName1();
+company1.getName2();
--- src/subject/debounce-throttling.js
+++ src/subject/debounce-throttling.js
@@ -1,3 +1,47 @@
+
 /**
  * 实现 防抖(debounce) 和 节流(throttling)
  */
+var count = 1
+var container = document.getElementById('container')
+function getUserAction(e) {
+  console.log(e)
+  container.innerHTML = count++
+}
+
+// container.onmousemove = getUserAction
+container.onmousemove = debounce(getUserAction, 1000)
+function debounce(fn, delay, immediate) {
+  var timeout
+  return function () {
+    let _this = this
+    let args = arguments
+    if(timeout) clearTimeout(timeout)
+    if (immediate) {
+      // 如果已经执行过不再执行
+      var callNow = !timeout
+      timeout = setTimeout(() => {
+        timeout = null
+      }, delay);
+      if (callNow) fn.apply(_this,args)
+    }else{
+      timeout = setTimeout(function () {
+        fn.apply(_this, args)
+      }, delay)
+    }
+    
+  }
+}
+function thor(fn,delay) {
+  var _this, args
+  var previous = 0
+  return function() {
+    var now = +new Date()
+    _this = this
+    args = arguments
+    if (now - previous > delay) {
+      fn.apply(this,args)
+      previous = now
+    }
+  }
+}
