8765396e7d6b8c6efff262b92f3fc1a7b58738a9
charis <tochariszhang@sina.com>
2021-06-28 09:59:57 +0800

    z

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
-function Company() {
-  var instance = {
-    name: 'a1',
-    getName() {
-      console.log(this.name);
-    },
-    getName1: () => {
-      console.log(this.name);
-    }
-  };
-  this.name = 'a2';
-  this.getName2 = function () {
-    console.log(this.name);
-  }
-  return instance;
+// function Company() {
+//   var instance = {
+//     name: 'a1',
+//     getName() {
+//       console.log(this.name);
+//     },
+//     getName1: () => {
+//       console.log(this.name);
+//     }
+//   };
+//   this.name = 'a2';
+//   this.getName2 = function () {
+//     console.log(this.name);
+//   }
+//   return instance;
+// }
+// Company.prototype.name = 'a3';
+// var company1 = new Company();
+// console.log(company1);
+// company1.getName();
+// company1.getName1();
+// company1.getName2();
+setTimeout(function () {
+  console.log("6");
+}, 0);
+console.log("1");
+async function async1() {
+  console.log("2");
+  await async2();
+  console.log("3");
 }
-Company.prototype.name = 'a3';
-var company1 = new Company();
-console.log(company1);
-company1.getName();
-company1.getName1();
-company1.getName2();
+async function async2() {
+  console.log("5");
+}
+// async1();
+console.log("4");
